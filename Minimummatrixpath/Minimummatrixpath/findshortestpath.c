a90660c89d1b66098d6149493dee799563dccfe0
Clanrat <karlmoritz92@gmail.com>
2015-11-09 19:00:23 +0100

    Minor changes

--- Minimummatrixpath/Minimummatrixpath/findshortestpath.c
+++ Minimummatrixpath/Minimummatrixpath/findshortestpath.c
@@ -2,15 +2,33 @@
 #include <stdlib.h>
 #include <string.h>
 
+/*
+Takes a file in the following format
+2
+2,3
+1,2
+3
+1,2,3
+4,5,6
+7,8,9
+...
+First number is the length of the nxn matrix the rest until the next length specifier is the values contained in the matrix
+
+*/
+
 
 typedef struct Path Path;
 
 struct Path /* Linked list of path coordinates*/
 {
-	int x, y, pathsum;
-	Path* next_path;
+	int x; //x-coordinate of the path
+	int	y; //y-coordinate of the path
+	int pathsum; // Sum of the steps passed over
+	Path* next_path; //Pointer to the next member of the linked list
 };
-
+/*
+Allocates memory for the matrix
+*/
 int ** allocMatrix(int length)
 {
 	int i;
@@ -22,7 +40,9 @@ int ** allocMatrix(int length)
 	}
 	return x;
 }
-
+/*
+Creates a new path linked list member and returns the pointer to it, by default the next path is set to null (appends to the end of the list)
+*/
 Path* newPath(int x, int y, int pathsum)
 {
 	Path* path_pointer;
@@ -37,20 +57,34 @@ Path* newPath(int x, int y, int pathsum)
 
 	return path_pointer;
 }
-
+/*Checks if the path can move both to the right and to the left
+0 - no
+1 - yes
+*/
 int checkSplit(int x, int y, int length)
 {
 	if ((x < length - 1) && (y < length - 1)) return 1;
 	return 0;
 }
 
+/*
+Finds the path with the smallest matrix sum by checking each possible permutation
+The algorithm starts at the topmost left corner. Each cycle it checks if it can move down or right, if both it splits into two path
+objects and moves the current path right otherwise it moves either right or down
 
+Returns the smallest matrix sum at completion
+
+*/
 int findShortestpath(int** matrix, int length)
 {
-	int xcurr, ycurr, smallest;
-	int end = 0;
-	int no_of_paths = 0;
-	Path *root, *currpath, *temp_path;
+	int xcurr; //Current paths x coordiant
+	int ycurr; //Current paths y coordinate
+	int smallest; //Smallest sum;
+	int end = 0; //Control variable for when all paths have reached the bottom right corner
+	int no_of_paths = 0; //Number of currently active paths (number of members in the linked list)
+	Path *root; //Root node in the linked list
+	Path *currpath; //Current node
+	Path *temp_path; //Temporary storage for a new node
 	root = newPath(0, 0, matrix[0][0]); /*Create path identifier in the top left corner*/
 	no_of_paths++;
 	currpath = root;
@@ -58,7 +92,7 @@ int findShortestpath(int** matrix, int length)
 	while (end != no_of_paths) /* Loop as long as all paths are not in the bottom right corner*/
 	{
 		end = 0;
-		do /*Loop through all paths*/
+		do /*Loop through all nodes*/
 		{
 			xcurr = currpath->x;
 			ycurr = currpath->y;
@@ -106,12 +140,12 @@ int findShortestpath(int** matrix, int length)
 		}
 		while ((currpath = currpath->next_path) != NULL); /*Loop through linked list of paths*/
 
-		currpath = root;
+		currpath = root; /*Reset linked list*/
 
 	}
 
 	smallest = root->pathsum;
-	do
+	do /*Loop through to check for the smallest sum*/
 	{
 		if (smallest > currpath->pathsum)
 		{
@@ -127,6 +161,11 @@ int main(int argc, char** args[])
 {
 
 	FILE* file = fopen(args[1], "r");
+	if(file == NULL)
+	{
+		printf("Cannot find file!");
+		return 0;
+	}
 	char line[1024];
 	char* tok;
 	int** matrix;
@@ -139,9 +178,7 @@ int main(int argc, char** args[])
 
 
 	while(fgets(line,1024,file))
-	{
-		
-		
+	{	
 		if( new_matrix == 1)
 		{
 			sscanf(line, "%d", &length);
@@ -173,13 +210,8 @@ int main(int argc, char** args[])
 			}
 			free(matrix);
 
-			new_matrix = 1;
-
-			
+			new_matrix = 1;	
 		}
-		
-		
 	}
-	
 	return 0;
 }
